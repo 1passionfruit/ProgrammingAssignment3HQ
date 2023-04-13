f8cf4beeabd85d4ae2e02e7a554da91ffcb3cacd
1passionfruit <mightyfinething@yahoo.com>
2023-04-12 22:32:12 -0500

    added sequentiall if statments Coded sorts stuck because R says "best" > 170. got to solve thats

--- rankhospital.R
+++ rankhospital.R
@@ -30,8 +30,53 @@ rankhospital <- function(state, outcome, num = "best") {
   
   ## Return hospital name in that state with the given rank
   
+  hName<-c()
+  hMR<-c()
+  hospWithOutcome<-data.frame(hsname=hName,mrate=hMR) ##empty data frame
   
+  ##process subset by extracting hospitals in the input state that has outcome data
   
+  for(index in 1:nrow(hospData)){
+    if((hospNamesForOutcome[index,2]==state) && 
+       (hospNamesForOutcome[index,3]!='Not Available')){
+      
+      hospWithOutcome<- rbind(hospWithOutcome, 
+                              data.frame(hsname=hospNamesForOutcome[index,1]
+                            ,mrate=as.numeric(hospNamesForOutcome[index,3])))
+      
+    }
+  }
+  
+  #
+  if(num == "best"){
+  ##sort first by mortality rate, then by hospital name
+  hospWithOutcome<-hospWithOutcome[order(hospWithOutcome$mrate, 
+                                         hospWithOutcome$hsname),]
+  print("again with the nonesense")
+  print(hospWithOutcome[1:3,])
+  }
+  if(num == "worst"){
+    hospWithOutcome<-hospWithOutcome[-order(hospWithOutcome$mrate, 
+                                           hospWithOutcome$hsname),]
+    print(hospWithOutcome[1:3,])
+  }
+  if(is.element(num, 1:nrow(hospWithOutcome))){
+    hospWithOutcome<-hospWithOutcome[order(hospWithOutcome$mrate, 
+                                           hospWithOutcome$hsname),]
+    
+    print(hospWithOutcome[1:num,] )
+    ##simple way
+                    
+    rankings<-(rank=c(1:nrow(hospWithOutcome)))
+    hospWithOutcome<-cbind(hospWithOutcome,rankings) #code gym
+    hospWithOutcome[num, ]
+    print(hospWithOutcome[1:num,] )
+  }
+  if(num>nrow(hospWithOutcome)){
+    print(nrow(hospWithOutcome))
+    print(num)
+    print("NA")
+  }
   
   ## 30-day death rate
 }
